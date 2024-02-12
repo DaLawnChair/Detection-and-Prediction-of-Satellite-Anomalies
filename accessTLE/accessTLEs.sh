fdbb3a4ab95bf85fe89d3ee50b2386272774b986
John Zhou <johnzhou2004@outlook.com>
2024-02-12 17:12:49 -0500

    Edited accessTLEs.sh and gathered data on satellites
     Changes to be committed:
    	deleted:    11.txt
    	deleted:    12.txt
    	deleted:    16.txt
    	deleted:    20.txt
    	modified:   accessTLEs.sh
    	deleted:    cookies.txt
    	deleted:    mega-data.txt
    	new file:   satelliteAddedInfo.txt

--- accessTLE/accessTLEs.sh
+++ accessTLE/accessTLEs.sh
@@ -15,32 +15,49 @@ password=""
 cookies="cookies.txt"
 
 #User set parameters for the range of satellites accessed
-startSatelliteId=1 
-endSatelliteId=20
+#Note using this method will lead to a bad if the difference between the start and end satellite ids is greater than ~400. 
+#Use with disgression.
+startSatelliteId=0
+endSatelliteId=300
 minLines=$(( 3650*2 )) #A mimimum of 10 years worth of data if the satellite produces 1 TLE per day
 dateStart="1957-01-01"
 dateEnd="2024-01-01"
 
 #combined output of the data from the satellites.
-combinedText="mega-data.txt"
+combinedData="combinedData.txt"
+sateliteInfo="satelliteAddedInfo.txt"
+rawDataHolderDir="rawDataHolder"
+
+#mkdir "$rawDataHolderDir"
 
 #Log in to space-track.org
 curl -c $cookies -b $cookies https://www.space-track.org/ajaxauth/login -d "identity=${user}&password=${password}"
 
-
 #Access all satellites within the given range.
 for(( satelliteNumber=${startSatelliteId}; satelliteNumber<=${endSatelliteId}; satelliteNumber++ ))
 do
-  #Access the TLE data for the satellite given the date range.
-  curl --cookie $cookies https://www.space-track.org/basicspacedata/query/class/gp_history/NORAD_CAT_ID/${satelliteNumber}/orderby/TLE_LINE1%20ASC/EPOCH/${dateStart}--${dateEnd}/format/tle --output ${satelliteNumber}.txt
+  if (( numOfLines == 68 )); then
+    curl -c $cookies -b $cookies https://www.space-track.org/ajaxauth/login -d "identity=${user}&password=${password}"
+    sleep 60
+  fi
 
+  #Access the TLE data for the satellite given the date range.
+  curl --cookie $cookies --limit-rate 100K https://www.space-track.org/basicspacedata/query/class/gp_history/NORAD_CAT_ID/${satelliteNumber}/orderby/TLE_LINE1%20ASC/EPOCH/${dateStart}--${dateEnd}/format/tle --output ${satelliteNumber}.txt
+  
+  numOfLines=$(wc -l < ${satelliteNumber}.txt) 
+  addedToCombinedData="No"
   #Remove the file if the amount of TLE lines does not meet the minimum.
-  if [[ $(wc -l < ${satelliteNumber}.txt) -lt ${minLines} ]]; then
+  if [[ $numOfLines -lt ${minLines} ]]; then
     rm ${satelliteNumber}.txt
   #Concatonate the data to the combinedText file.
   else
-    cat ${satelliteNumber}.txt >> ${combinedText}
+    cat ${satelliteNumber}.txt >> ${combinedData}
+    addedToCombinedData="Yes"
+    mv ${satelliteNumber}.txt "$rawDataHolderDir"
   fi
+
+  # Store information on the satellites entered
+  echo "$satelliteNumber    $numOfLines   $addedToCombinedData" >> "$sateliteInfo"
 done
 
 
